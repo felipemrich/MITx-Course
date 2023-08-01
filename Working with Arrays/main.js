8451974aacd58cd821bb79f0b09720efe31dd407
Felipe Emrich <felipeemrichdearaujo@hotmail.com>
2023-08-01 18:15:56 -0300

    Update main.js

--- Working with Arrays/main.js
+++ Working with Arrays/main.js
@@ -1,9 +1,8 @@
-// declaring an array of numbers 1 through 10.
-
+// Declare an array containing numbers from 1 to 10.
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let elementAtIndexFive = numArray[5];
+// Access the element at index 5 (6th element) of the array and store it in a variable.
+let elementAtIndexFive = numArray[5]; // Index 5 corresponds to the number 6.
 
-//open the browser console to check results
-console.log('The array element with index 5 is: ', elementAtIndexFive);
-
+// Output the element at index 5 (6th element) of the array to the browser console.
+console.log('The array element with index 5 is: ', elementAtIndexFive); // Output: The array element with index 5 is: 6
