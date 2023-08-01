839c587e702ba8d74db6b5e07b64880e43e13144
Felipe Emrich <felipeemrichdearaujo@hotmail.com>
2023-08-01 18:14:35 -0300

    Update scope.js

--- Scope/scope.js
+++ Scope/scope.js
@@ -1,15 +1,18 @@
+// Declare a global variable called "animal" and set it to 'dog'.
 var animal = 'dog';
 
+// Function "myPet" returns the value of the global variable "animal".
 function myPet() {
   return animal;
 }
 
+// Function "yourPet" declares a local variable called "animal" and sets it to 'cat'.
+// This local variable shadows the global variable "animal" within this function's scope.
 function yourPet() {
   let animal = 'cat';
   return animal;
 }
 
-//open your browser console to check the results
-console.log('myPet result: ' + myPet());
-console.log('yourPet result: ' + yourPet());
-
+// Output the results of calling both functions to the browser console.
+console.log('myPet result: ' + myPet()); // Output: myPet result: dog
+console.log('yourPet result: ' + yourPet()); // Output: yourPet result: cat
